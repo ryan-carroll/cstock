a94a070cdd8a5626be573aa065bf356cc9747cc7
Ryan <carroll.ryan@gmail.com>
2016-05-16 20:41:34 -0400

    Add files via upload
    initial upload

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+process.env.PWD = process.cwd();
+app.set('views', path.join(process.env.PWD, 'src'));
+app.use(express.static(path.join(process.env.PWD, 'src')));
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(port, function() {
+    console.log("Server is running on port " + port);
+});
